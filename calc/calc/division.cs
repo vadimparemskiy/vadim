b3f04eacc85e9a87f507cb5fd023ffc04cbe8679
maksimkirillov <max.kirillov@mail.ru>
2018-07-03 14:28:32 +0600

    establishment of a factory

---
+++ calc/calc/division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public class division : ICalculateTwoArguments
+    {
+        public double Calculate(double firstArgument, double secondArgument)
+        {
+            return firstArgument / secondArgument;
+        }
+    }
+}
